a6189a37d613eea74d57888e4b71a973df800aed
Jhonnathan Ricardo Albarracin Peralta <ingeo.jhonap@gmail.com>
2024-05-20 18:26:57 +0200

    PBI #8 game start scene

---
+++ src/ecs/components/c_timer.py
@@ -0,0 +1,5 @@
+
+class CTimer:
+    def __init__(self, duration: float):
+        self.accumulated_time = 0.0
+        self.duration = duration
--- src/game/play_scene.py
+++ src/game/play_scene.py
@@ -13,8 +13,8 @@ from configurations.global_config import GlobalConfig
 from src.create.prefab_creator_game import (create_bullet, create_enemies_grid,
                                             create_enemy, create_game_input,
                                             create_player, create_score, create_stars)
-from src.create.prefab_creator_interface import (TextAlignment,
-                                                 create_blink_text,
+from src.create.prefab_creator_interface import (TextAlignment, create_1up_text,
+                                                 create_blink_text, create_score_text,
                                                  create_text)
 from src.ecs.components.c_input_command import CInputCommand, CommandPhase
 from src.ecs.components.c_surface import CSurface
@@ -55,7 +55,7 @@ class PlayScene(Scene):
         self.player_entity = create_player(self.ecs_world,
                                            self.config.player,
                                            self._game_engine.screen_props)
-        
+
         self._p_v = self.ecs_world.component_for_entity(
             self.player_entity, CVelocity)
 
@@ -65,16 +65,13 @@ class PlayScene(Scene):
         self._p_s = self.ecs_world.component_for_entity(
             self.player_entity, CSurface)
 
-        #score
-        score_header_text = create_text(self.ecs_world, "1-UP", 8,
-                                      interface["title_text"].color,
-                                      Position(self._game_engine.screen_props.top_left.x + 5, self._game_engine.screen_props.top_left.y + 5),
-                                      TextAlignment.LEFT)
-        self.score_text_ent = create_text(self.ecs_world, "000000", 8, #initial fixed width
-                                      interface["normal_text"].color,
-                                      Position(self._game_engine.screen_props.top_left.x + 10, self._game_engine.screen_props.top_left.y + 15),
-                                      TextAlignment.LEFT)
-        self.p_txt_score = self.ecs_world.component_for_entity(paused_text_ent, CSurface)
+        # score
+        score_header_text = create_1up_text(
+            self.ecs_world, interface, self._game_engine.screen_props)
+        self.score_text_ent = create_score_text(
+            self.ecs_world, interface, self._game_engine.screen_props)
+        self.p_txt_score = self.ecs_world.component_for_entity(
+            paused_text_ent, CSurface)
         self.score_entity = create_score(self.ecs_world)
         create_game_input(self.ecs_world)
         create_stars(self.ecs_world, self.config.starfield,
@@ -95,7 +92,8 @@ class PlayScene(Scene):
             system_blink(self.ecs_world, delta_time)
             system_movement(self.ecs_world, delta_time)
             system_screen_bullet(self.ecs_world, self.screen_rect)
-            system_collision_bullet_enemy(self.ecs_world, self.config.enemy_explosion, self.score_entity)
+            system_collision_bullet_enemy(
+                self.ecs_world, self.config.enemy_explosion, self.score_entity)
             system_player_state(self.ecs_world)
             system_refresh_score(self.ecs_world, self.score_text_ent)
             system_animation(self.ecs_world, delta_time)
