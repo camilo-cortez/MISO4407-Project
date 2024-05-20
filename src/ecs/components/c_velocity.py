9fbb8b0f23fa8e960279f4b28393d38645378d23
Jhonnathan Ricardo Albarracin Peralta <ingeo.jhonap@gmail.com>
2024-05-20 19:44:48 +0200

    PBI #7 ajuste de blink paused y dispara de jugador con la tecla z

--- src/ecs/components/c_velocity.py
+++ src/ecs/components/c_velocity.py
@@ -1,5 +1,7 @@
 import pygame
 
+
 class CVelocity:
-    def __init__(self, vel:pygame.Vector2) -> None:
+    def __init__(self, vel: pygame.Vector2, move_in_paused: bool = False) -> None:
         self.vel = vel
+        self.move_in_paused = move_in_paused
